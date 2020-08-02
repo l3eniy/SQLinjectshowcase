9015d14876412ad4be787527622823ac7da513c0
Benjamin Fuhlbrügge <s176110@hft-leipzig.de>
2020-08-02 14:32:58 +0200

    wdw

--- app.py
+++ app.py
@@ -49,10 +49,11 @@ def query():
             results = database.select(statement_u)
             print(password)
             print(str(results[0][0]))
-            retval = list()
-            for result in results:
-                retval.append(str(result))
-            return '<br>'.join(retval)
+            if password != str(results[0][0]):
+                ergebnis = "Success !"
+            else:
+                ergebnis = "Username or Password wrong !"
+            return '<br>'.ergebnis
 
 
 if __name__ == '__main__':
